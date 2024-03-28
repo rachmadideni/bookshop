05fdbe4d017ddf963683999a5181dc849cbac4c1
rachmadideni <rachmadideni@gmail.com>
2024-03-28 23:27:36 +0800

    remove unused components & layouts

--- client/src/app/auth/login/layout.tsx
+++
@@ -1,9 +0,0 @@
-type AuthLayoutProps = {
-  children: React.ReactNode;
-};
-
-const AuthLayout = (props: AuthLayoutProps) => {
-  return <div>auth layout{props.children}</div>;
-};
-
-export default AuthLayout;
--- client/src/app/auth/login/page.tsx
+++
@@ -1,9 +0,0 @@
-const AuthPage = () => {
-  return (
-    <div className="flex flex-col w-full h-screen items-center justify-center">
-      <h1 className="font-poppins">Auth Page</h1>
-    </div>
-  );
-};
-
-export default AuthPage;
