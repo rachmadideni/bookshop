2ea857cab5ec582e27ff7507a9ac6f3d277b71cb
rachmadideni <rachmadideni@gmail.com>
2024-03-28 23:34:40 +0800

    update homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import Books from "@/features/books";
 import BookFilter from "@/components/book-filter";
+import useInfiniteBooks from "@/hooks/useInfiniteBooks";
 
 export default function Home() {
+  const infiniteQueries = useInfiniteBooks();
+
   return (
     <>
-      <div className="flex flex-row w-full md:space-x-12">
-        <div className="grow-0 w-80 hidden md:block">
-          <BookFilter />
+      <div className="flex flex-col md:flex-row w-full md:space-x-12">
+        <div className="grow-0 w-full md:w-80 md:block">
+          <BookFilter fetchNextPage={infiniteQueries.fetchNextPage} />
         </div>
         <div className="flex-2 grow-0 ">
-          <Books />
+          <Books {...infiniteQueries} />
         </div>
       </div>
     </>
