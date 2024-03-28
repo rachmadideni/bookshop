60f4bb2922740ab9755afc615409521917d526a2
rachmadideni <rachmadideni@gmail.com>
2024-03-28 23:42:13 +0800

    update database config

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -3,17 +3,17 @@ import { SeederOptions } from "typeorm-extension";
 import { BookFactory } from "./factories/book.factory";
 import { CustomerFactory } from "./factories/customer.factory";
 import { MainSeeder } from "./seeders/main.seeder";
+import path from "path";
 
 export const options: DataSourceOptions & SeederOptions = {
   type: "postgres",
-  host: "localhost",
   port: Number(process.env.DB_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  entities: [__dirname + "/../entities/*.{ts,js}"],
+  entities: [path.join(__dirname + "/../entities/*.{ts,js}")],
   logging: true,
-  synchronize: process.env.NODE_ENV === "development",
+  synchronize: false,
   factories: [BookFactory, CustomerFactory],
   seeds: [MainSeeder],
 };
