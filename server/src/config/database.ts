7b73ef7d69708afa8588c1e6c484c455205419a5
rachmadideni <rachmadideni@gmail.com>
2024-04-01 09:39:36 +0800

    chore: add db host prod

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -7,6 +7,7 @@ import path from "path";
 
 export const options: DataSourceOptions & SeederOptions = {
   type: "postgres",
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
